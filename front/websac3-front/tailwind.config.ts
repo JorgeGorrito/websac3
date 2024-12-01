a5fd3628847a7529ad7865f1b4113445549ec19f
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2024-12-01 01:52:56 -0500

    conf(front): add colors palette as tailwind class

--- front/websac3-front/tailwind.config.ts
+++ front/websac3-front/tailwind.config.ts
@@ -9,8 +9,26 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          light: '#1E4E79', // Azul principal
+          dark: '#122A45',  // Azul más oscuro para modo oscuro
+        },
+        accent: {
+          light: '#E53935', // Rojo brillante (modo claro)
+          dark: '#C62828',  // Rojo oscuro (modo oscuro)
+        },
+        background: {
+          light: '#FFFFFF', // Fondo blanco (modo claro)
+          dark: '#121212',  // Fondo oscuro (modo oscuro)
+        },
+        secondary: {
+          light: '#3B87C1', // Azul claro (modo claro)
+          dark: '#246791',  // Azul claro más oscuro
+        },
+        neutral: {
+          light: '#E5E5E5', // Gris claro (modo claro)
+          dark: '#1A1A1A',  // Gris oscuro (modo oscuro)
+        },
       },
     },
   },
