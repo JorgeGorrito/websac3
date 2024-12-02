bc819cb7928577fab9fe3315e236cc2035447594
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2024-12-01 23:47:06 -0500

    feat(front/layout): create default layout styles

--- front/websac3-front/src/layouts/DefaultLayout.tsx
+++ front/websac3-front/src/layouts/DefaultLayout.tsx
@@ -1,12 +1,23 @@
-import WebSAC3Header from "@/components/websac3-header/websac3Header";
+import { WebSAC3Footer } from "@/components/websac3/footer/WebSAC3Footer";
+import WebSAC3Header from "@/components/websac3/header/WebSAC3Header";
+import "@/styles/layouts/DefaultLayout.css";
+import Head from "next/head";
 import React from "react";
 
 const DefaultLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <>
-      <WebSAC3Header />
-      {children}
-    </>
+      <>
+        <Head>
+            <title>WebSAC3</title>
+            <meta name="description" content="Web System for analysis of curricula cybersecurity component" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <main className="main-container">
+          <WebSAC3Header/>
+            {children}
+          <WebSAC3Footer/>
+        </main>
+      </>
   );
 };
 
