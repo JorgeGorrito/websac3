8c2fa25ab25215586046928d9880ad919c651d25
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2024-12-02 00:10:27 -0500

    feat(front): add websac3 logo component

---
+++ front/websac3-front/src/components/websac3/logos/WebSAC3Logo.tsx
@@ -0,0 +1,21 @@
+import LogoWebSAC3 from "@/../public/websac3/logo-websac3.png";
+import Image from "next/image";
+import "@/styles/websac3/logos/WebSAC3Logo.css";
+
+const WebSAC3Logo = () => {
+    return (
+        <div className="webSAC3Logo">
+            <Image
+            src={LogoWebSAC3}
+            alt="Logo WebSAC3"
+            width={1600} 
+            height={516}
+            className="h-[95%] w-auto"
+            quality={100}
+            draggable={false}
+            />
+      </div>
+    );
+}
+
+export { WebSAC3Logo };
