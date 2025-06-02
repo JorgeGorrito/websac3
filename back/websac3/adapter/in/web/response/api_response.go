35009fa447bef435157289a30009a9b9850b72aa
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2025-06-01 23:55:53 -0500

    feat(back/apiresponse): add error field

--- back/websac3/adapter/in/web/response/api_response.go
+++ back/websac3/adapter/in/web/response/api_response.go
@@ -3,10 +3,12 @@ package response
 type ApiResponse[T any] struct {
 	HttpStatusCode int
 	Result         T
+	Error          string
 }
 
-func (r *ApiResponse[T]) ToResponseFormat() map[string]T {
-	return map[string]T{
+func (r *ApiResponse[T]) ToResponseFormat() map[string]any {
+	return map[string]any{
 		"result": r.Result,
+		"error":  r.Error,
 	}
 }
