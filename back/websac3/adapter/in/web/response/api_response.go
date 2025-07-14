84bf53b1eaca7f8451ab1a1ee86e5b6eb9c0250b
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2025-07-14 12:43:09 -0500

    refactor(back/apiresponse): improve API response structure

--- back/websac3/adapter/in/web/response/api_response.go
+++ back/websac3/adapter/in/web/response/api_response.go
@@ -3,12 +3,12 @@ package response
 type ApiResponse[T any] struct {
 	HttpStatusCode int
 	Result         T
-	Error          string
+	Errors         []string
 }
 
 func (r *ApiResponse[T]) ToResponseFormat() map[string]any {
 	return map[string]any{
 		"result": r.Result,
-		"error":  r.Error,
+		"errors": r.Errors,
 	}
 }
