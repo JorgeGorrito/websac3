29b829f699e3babab417167e655c369d5da7f68e
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2025-07-14 10:21:31 -0500

    refactor(back/mapper): improve mapper util structure

--- back/websac3/common/mapper/mapper.go
+++ back/websac3/common/mapper/mapper.go
@@ -6,133 +6,65 @@ import (
 	"reflect"
 )
 
-func setSrcKeysValues(tagToField *map[string]reflect.Value, srcValue reflect.Value) {
-	var srcType reflect.Type = srcValue.Type()
-	if srcValue.Kind() == reflect.Ptr {
-		srcValue = srcValue.Elem()
-		srcType = srcValue.Type()
-	}
+var (
+	SrcPointerIsNilError = errors.New("source pointer is nil")
+)
 
-	var field reflect.StructField
-	var fieldTag string
-	var srcValueField reflect.Value
-	var isSrcValueFieldPtrToStruct bool
-	var isSrcValuesAStruct bool
-	for i := 0; i < srcType.NumField(); i++ {
-		field = srcType.Field(i)
-		fieldTag = field.Tag.Get("mapper")
+type MapFunc func(sourcePtr any) (any, error)
 
-		if fieldTag == "" {
-			continue
+var registry map[reflect.Type]map[reflect.Type]MapFunc = make(map[reflect.Type]map[reflect.Type]MapFunc)
+
+func BuildMapFunc[S any, D any](fn func(*S) (D, error)) MapFunc {
+	return func(sourcePtr any) (any, error) {
+		typedSrc, ok := sourcePtr.(*S)
+		if !ok {
+			return nil, fmt.Errorf("invalid source type: expected *%T", new(S))
 		}
-
-		srcValueField = srcValue.Field(i)
-		isSrcValueFieldPtrToStruct = srcValueField.Kind() == reflect.Ptr && !srcValueField.IsNil() && srcValueField.Elem().Kind() == reflect.Struct
-		isSrcValuesAStruct = srcValueField.Kind() == reflect.Struct
-		if isSrcValueFieldPtrToStruct || isSrcValuesAStruct {
-			setSrcKeysValues(tagToField, srcValueField)
-		}
-		(*tagToField)[fieldTag] = srcValueField
+		return fn(typedSrc)
 	}
 }
 
-func mapWithoutOverLoadTags(src, dest any, tagToField *map[string]reflect.Value) error {
-	var srcValue reflect.Value = reflect.ValueOf(src)
-	var destValue reflect.Value = reflect.ValueOf(dest)
+func RegisterMapFunc[S any, D any](fn func(*S) (D, error)) {
+	var zero D
+	srcType := reflect.TypeOf((*S)(nil)).Elem()
+	dstType := reflect.TypeOf(zero)
 
-	if destValue.Kind() != reflect.Ptr || destValue.Elem().Kind() != reflect.Struct {
-		return errors.New("dest must be a pointer to a struct")
+	if _, ok := registry[srcType]; !ok {
+		registry[srcType] = make(map[reflect.Type]MapFunc)
 	}
-
-	if srcValue.Kind() != reflect.Ptr || srcValue.Elem().Kind() != reflect.Struct {
-		return errors.New("src must be a pointer to a struct")
-	}
-
-	var destElem reflect.Value = destValue.Elem()
-	if tagToField == nil {
-		tagToField = &map[string]reflect.Value{}
-	}
-	setSrcKeysValues(tagToField, srcValue)
-
-	var destType reflect.Type = destElem.Type()
-	var field reflect.StructField
-	var tag string
-	var destField reflect.Value
-	for i := 0; i < destElem.NumField(); i++ {
-		field = destType.Field(i)
-		tag = field.Tag.Get("mapper")
-		if tag == "" {
-			continue
-		}
-
-		srcField, exists := (*tagToField)[tag]
-		if !exists || !field.IsExported() {
-			continue
-		}
-
-		destField = destElem.Field(i)
-		// Si los tipos son directamente asignables
-		if srcField.Type().AssignableTo(destField.Type()) {
-			destField.Set(srcField)
-			continue
-		}
-
-		if srcField.Type().ConvertibleTo(destField.Type()) {
-			destField.Set(srcField.Convert(destField.Type()))
-			continue
-		}
-
-		// Si ambos son estructuras (o punteros a estructuras), intentamos mapearlas recursivamente
-		srcFieldType := srcField.Type()
-		destFieldType := destField.Type()
-
-		// Manejar punteros a estructuras
-		if srcFieldType.Kind() == reflect.Ptr && srcFieldType.Elem().Kind() == reflect.Struct &&
-			destFieldType.Kind() == reflect.Ptr && destFieldType.Elem().Kind() == reflect.Struct {
-
-			if srcField.IsNil() {
-				continue // o manejar el caso nil como prefieras
-			}
-
-			if destField.IsNil() {
-				destField.Set(reflect.New(destFieldType.Elem()))
-			}
-
-			continue
-		}
-
-		// Manejar estructuras a punteros
-		if srcFieldType.Kind() == reflect.Struct && destFieldType.Kind() == reflect.Ptr && destFieldType.Elem().Kind() == reflect.Struct {
-			if destField.IsNil() {
-				destField.Set(reflect.New(destFieldType.Elem()))
-			}
-			if err := mapWithoutOverLoadTags(srcField.Addr().Interface(), destField.Interface(), tagToField); err != nil {
-				return err
-			}
-			continue
-		}
-
-		// Manejar estructuras directas (no punteros)
-		if srcFieldType.Kind() == reflect.Struct && destFieldType.Kind() == reflect.Struct {
-			// Creamos un nuevo valor para el destino
-			newDestValue := reflect.New(destFieldType)
-			if err := mapWithoutOverLoadTags(srcField.Addr().Interface(), newDestValue.Interface(), tagToField); err != nil {
-				return err
-			}
-			destField.Set(newDestValue.Elem())
-			continue
-		}
-
-		return fmt.Errorf(
-			"incompatible types for tag '%s': %s vs %s",
-			tag,
-			srcField.Type(),
-			destField.Type(),
-		)
-	}
-	return nil
+	registry[srcType][dstType] = BuildMapFunc(fn)
 }
 
-func Map(src, dest any) error {
-	return mapWithoutOverLoadTags(src, dest, nil)
+func Map[S any, D any](sourcePtr *S) (D, error) {
+	var zero D
+
+	if sourcePtr == nil {
+		return zero, SrcPointerIsNilError
+	}
+
+	srcType := reflect.TypeOf((*S)(nil)).Elem()
+	var d D
+	dstType := reflect.TypeOf(d)
+
+	mapFuncs, found := registry[srcType]
+	if !found {
+		return zero, fmt.Errorf("no mapping functions registered for type %v", srcType)
+	}
+
+	mapFunc, found := mapFuncs[dstType]
+	if !found {
+		return zero, fmt.Errorf("no mapping function from %v to %v", srcType, dstType)
+	}
+
+	result, err := mapFunc(sourcePtr)
+	if err != nil {
+		return zero, err
+	}
+
+	typedResult, ok := result.(D)
+	if !ok {
+		return zero, fmt.Errorf("invalid result type")
+	}
+
+	return typedResult, nil
 }
