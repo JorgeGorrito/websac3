a2a5a910d88f818d54595b7b37df407f37384742
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2025-07-14 11:59:58 -0500

    refactor(back/handler): remove base handler and update create access request handler

--- back/websac3/adapter/in/web/handler/command/create_access_request_command_handler.go
+++ back/websac3/adapter/in/web/handler/command/create_access_request_command_handler.go
@@ -3,10 +3,13 @@ package command
 import (
 	"net/http"
 	"websac3/adapter/in/web/response"
+	"websac3/adapter/in/web/util"
 	"websac3/app/domain/entity"
 	"websac3/app/port/in/dto/command"
 	"websac3/app/port/in/usecase"
-	"websac3/app/port/out/logging"
+	"websac3/common/logging"
+	"websac3/common/mapper"
+	"websac3/common/validator"
 )
 
 type CreateAccessRequestCommandHandler struct {
@@ -24,25 +27,40 @@ func NewCreateAccessRequestCommandHandler(
 	}
 }
 
-func (h *CreateAccessRequestCommandHandler) Handle(request command.CreateAccessRequestCommand) (response.ApiResponse[string], error) {
-	return BaseApiHandler[command.CreateAccessRequestCommand, entity.AccessRequest](
-		request,
-		h.createAccessRequestUseCase,
-		h.logger,
-		MessageBaseApiHandler{
-			Logger: LoggerMessages{
-				InitMessage:          "Inicio la creación de solicitud de acceso para el usuario con CC: " + request.Person.IdentificationNumber,
-				InputValidationError: "Error de validación datos de entrada al crear solicitud de acceso.",
-				MappingError:         "Error al mapear datos de entrada a entidad AccessRequest.",
-				SuccessMessage:       "Solicitud de acceso creada exitosamente para el usuario con CC: " + request.Person.IdentificationNumber,
-				UseCaseError:         "Error al crear solicitud de acceso para el usuario con CC " + request.Person.IdentificationNumber + ".",
-			},
-			Api: ApiMessages{
-				SuccessMessage: "Solicitud de acceso creada exitosamente",
-				MappingError:   "Error al procesar los datos de la solicitud de acceso",
-				UseCaseError:   "Algo salió mal al registrar la solicitud de acceso",
-			},
-		},
-		http.StatusCreated,
-	), nil
+func (h *CreateAccessRequestCommandHandler) Handle(request command.CreateAccessRequestCommand, lang string) (response.ApiResponse[string], error) {
+	h.logger.Info("Inicio la creación de solicitud de acceso para el usuario con CC: " + request.Person.IdentificationNumber)
+	if err := validator.ValidateFields(&request); err != nil {
+		h.logger.Error("Error de validación datos de entrada al crear solicitud de acceso. Errores: %v", err)
+		var validationErrors []string
+		for n := range err {
+			validationErrors = append(validationErrors, err[n].Error())
+		}
+		return response.ApiResponse[string]{
+			HttpStatusCode: http.StatusBadRequest,
+			Errors:         validationErrors,
+		}, nil
+	}
+
+	accessRequest, err := mapper.Map[command.CreateAccessRequestCommand, entity.AccessRequest](&request)
+	if err != nil {
+		h.logger.Error("Error al mapear datos de entrada a entidad AccessRequest. Error: %s", err.Error())
+		return response.ApiResponse[string]{
+			HttpStatusCode: http.StatusInternalServerError,
+			Errors:         []string{"Error al procesar los datos de la solicitud de acceso"},
+		}, nil
+	}
+
+	if err := h.createAccessRequestUseCase.Execute(accessRequest, request.RedirectUrlTo, lang); err != nil {
+		h.logger.Error("Error al crear solicitud de acceso para el usuario con CC "+request.Person.IdentificationNumber+". Error: %s", err.Error())
+		var httpStatusCode int = util.GetHttpStatusCodeByErr(err)
+		return response.ApiResponse[string]{
+			HttpStatusCode: httpStatusCode,
+			Errors:         []string{util.GetResultMessageByErr(err, "Algo salió mal al registrar la solicitud de acceso")},
+		}, nil
+	}
+	h.logger.Info("Solicitud de acceso creada exitosamente para el usuario con CC: " + request.Person.IdentificationNumber)
+	return response.ApiResponse[string]{
+		HttpStatusCode: http.StatusOK,
+		Result:         "Solicitud de acceso creada exitosamente",
+	}, nil
 }
