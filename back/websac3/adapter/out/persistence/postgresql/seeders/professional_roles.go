78c685419add03124944c1bae700e79628bd4359
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2026-02-06 09:07:10 -0500

    fix(back/seeder): correct professional roles bug

--- back/websac3/adapter/out/persistence/postgresql/seeders/professional_roles.go
+++ back/websac3/adapter/out/persistence/postgresql/seeders/professional_roles.go
@@ -40,7 +40,6 @@ func (p *professionalRoles) Seed(ctx db.Context) error {
 	ResetAutoIncrement(ctx, "professional_roles")
 	ResetAutoIncrement(ctx, "professional_role_knowledge_areas")
 	ResetAutoIncrement(ctx, "professional_role_knowledge_area_topics")
-	ResetAutoIncrement(ctx, "degree_program_professional_roles")
 
 	return nil
 }
